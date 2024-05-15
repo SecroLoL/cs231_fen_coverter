2cbf8f040320b9bc2f565b5f45b950016ebd4c3d
SecroLoL <azshan@stanford.edu>
2024-05-15 13:48:45 -0700

    Build a baseline CNN that performs occupancy detection on 100 x 100 images, 3 layer ConvNet

---
+++ occupancy_detection/baseline_cnn.py
@@ -0,0 +1,37 @@
+import torch.nn.functional as F
+import functools
+import sys 
+import os 
+
+from torch import nn
+from torchvision import models
+
+class CNN_100(nn.Module):
+    """
+    CNN with input size 100, 3 conv layers, 3 pooling layers, and 3 FC layers
+    """
+    input_size = 100, 100
+    pretrained = False
+
+    def __init__(self):
+        super().__init__()
+        # Input size: 100x100
+        self.conv1 = nn.Conv2d(3, 16, 5)  # 96
+        self.pool1 = nn.MaxPool2d(2, 2)  # 48
+        self.conv2 = nn.Conv2d(16, 32, 5)  # 44
+        self.pool2 = nn.MaxPool2d(2, 2)  # 22
+        self.conv3 = nn.Conv2d(32, 64, 3)  # 20
+        self.pool3 = nn.MaxPool2d(2, 2)  # 10
+        self.fc1 = nn.Linear(64 * 10 * 10, 1000)
+        self.fc2 = nn.Linear(1000, 256)
+        self.fc3 = nn.Linear(256, 2)   # 2 classes
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = x.view(-1, 64 * 10 * 10)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x

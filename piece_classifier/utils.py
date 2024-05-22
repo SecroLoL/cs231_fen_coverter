8c653069b30eb1f7b6dfe7b48d73d16240e94393
SecroLoL <azshan@stanford.edu>
2024-05-21 21:37:25 -0700

    Add method to load dataset singly instead of loading both the train and test sets simultaneously. Used for eval

--- piece_classifier/utils.py
+++ piece_classifier/utils.py
@@ -1,33 +1,14 @@
-"""
-General utility functions for all code in this dir
-"""
 import os 
+import sys 
 
-def generate_checkpoint_path(save_path: str):
+# TODO: add this to PYTHONPATH so that this isn't an issue
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # sys path issues, this is a quick fix for now
 
-    """
-    Given a model path, create a checkpoint path by appending '_ckpt' to the filename.
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, Subset
+from piece_classifier.model_types import ModelType
+import numpy as np
 
-    Args:
-    model_path (str): The original path to the model file.
-
-    Returns:
-    str: The new path with '_ckpt' appended to the filename.
-    """
-    # Extract the directory and filename
-    dir_name = os.path.dirname(save_path)
-    base_name = os.path.basename(save_path)
-    
-    # Split the filename into name and extension
-    name, ext = os.path.splitext(base_name)
-    
-    # Create the new filename with '_ckpt' appended
-    new_base_name = f"{name}_ckpt{ext}"
-    
-    # Construct the new path
-    new_path = os.path.join(dir_name, new_base_name)
-    
-    return new_path
 
 def load_dataset(model_type: ModelType, data_path: str, batch_size: int = 32, subset_size: int = None):
 

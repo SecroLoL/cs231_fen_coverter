77f9c6ea7ad7e623ab5d238aaa8103d5e95fa4af
SecroLoL <azshan@stanford.edu>
2024-06-01 17:21:43 -0700

    Start transfer learning code

---
+++ transfer_learning/finetune.py
@@ -0,0 +1,11 @@
+"""
+Finetune a trained occupancy or piece classifier on a transfer learning dataset.
+
+"""
+
+# Load the transfer dataset
+
+# Train loop
+# Load model
+# loop over several epochs
+# 

8dd9ab3d191a53d805d1a1de95eabb4e6a351e06
SecroLoL <azshan@stanford.edu>
2024-06-02 18:42:42 -0700

    add documentation for util fns

--- piece_classifier/utils.py
+++ piece_classifier/utils.py
@@ -1,8 +1,7 @@
 import os 
 import sys 
 
-# TODO: add this to PYTHONPATH so that this isn't an issue
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # sys path issues, this is a quick fix for now
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # Put the root dir as part of the PYTHONPATH
 
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, Subset
@@ -10,10 +9,20 @@ from piece_classifier.model_types import ModelType
 import numpy as np
 
 
-def load_dataset(model_type: ModelType, data_path: str, batch_size: int = 32, subset_size: int = None):
+def load_dataset(model_type: ModelType, data_path: str, batch_size: int = 32, subset_size: int = None) -> DataLoader:
 
     """
-    TODO
+    Creates a DataLoader for a provided dataset, adjusting input sizes and shapes according to which model is 
+    being trained.
+
+    Args: 
+        model_type (ModelType): The ModelType class of the model that this dataset is used for. This is included to support the INCEPTION class, which requires addtl preprocessing.
+        data_path (str): The path to the data directory containing examples.
+        batch_size (int): Size of each batch of examples
+        subset_size (int, optional): If not using the entire dataset for the DataLoader, the number of examples to sample from the dataset. Defaults to all examples.
+
+    Returns:
+        A PyTorch DataLoader object that can be iterated to obtain the examples and labels.
     """
 
     if model_type is None:
@@ -23,7 +32,7 @@ def load_dataset(model_type: ModelType, data_path: str, batch_size: int = 32, su
         transforms.ToTensor()
     ])
 
-    if model_type == ModelType.INCEPTION:
+    if model_type.value == ModelType.INCEPTION.value:
         transform = transforms.Compose([
         transforms.Resize((299, 299)),  
         transforms.ToTensor(),
